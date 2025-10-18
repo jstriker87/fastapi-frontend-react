f712df7ceea391abf7724a5c1266f6558a1f2525
James Bart <jbart1@gmail.com>
2025-10-18 14:46:54 +0100

    Added tabs. About to move everything to correct directories

--- secrets-frontend/new-frontend/src/App.jsx
+++ secrets-frontend/new-frontend/src/App.jsx
@@ -1,23 +1,35 @@
-import React from 'react';
 import SecretList from './components/SecretList';
 import SecretForm from './components/SecretForm';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
     <>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
-    <div className="App">
-      <h1>Secrets Management</h1>
-      <SecretForm />
-      <SecretList />
-    </div>
-  </>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="logo" alt="logo" />
+        </header>
+      </div>
+
+      <div className="App">
+        <h1>Secrets Management</h1>
+<Tabs>
+    <TabList>
+      <Tab>Add Secret</Tab>
+      <Tab>Check Secret</Tab>
+    </TabList>
+
+    <TabPanel>
+        <SecretForm />
+    </TabPanel>
+    <TabPanel>
+        <SecretList />
+    </TabPanel>
+  </Tabs>
+      </div>
+    </>
   );
 }
 
